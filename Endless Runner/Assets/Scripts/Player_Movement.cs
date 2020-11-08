e9489fce6a625e7fb0c91f2e1c75c1c095c6bec4
SamTech1712 <74134203+SamTech1712@users.noreply.github.com>
2020-11-08 18:10:58 +0530

    added jump script

---
+++ Endless Runner/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float jumpForce;
+    
+    public Rigidbody2D rb2D;
+
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb2D = transform.GetComponent <Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb2D.AddForce(Vector3.up * jumpForce * 20f);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "ground")
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
